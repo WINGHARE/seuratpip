7de4962b0e1950b50175a1dd450f662869c50796
junyichen8 <junyichen8-c@my.cityu.edu.hk>
2019-07-06 03:55:51 +0800

    initial

--- ins.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# My first script
-#dates = $(date +%s)
-Rscript monoins.R
--- monoins.R
+++
@@ -1,4 +0,0 @@
-BiocManager::install(c('BiocGenerics', 'DelayedArray', 'DelayedMatrixStats', 
-                       'limma', 'S4Vectors', 'SingleCellExperiment', 
-                       'SummarizedExperiment'))
-devtools::install_github('cole-trapnell-lab/monocle3')
