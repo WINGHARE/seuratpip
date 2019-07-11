b7569fe82ca60c28d7e0573596cfbb980e061857
junyichen8 <junyichen8-c@my.cityu.edu.hk>
2019-07-10 22:30:18 -0400

    Edit the file figname

--- integration.R
+++ integration.R
@@ -76,22 +76,25 @@ labels.files.num <- unlist(lapply(rownames(data.all.integrated@meta.data),
 data.all.integrated@meta.data<-cbind(data.all.integrated@meta.data,labels.files.num)
 
 saveRDS(data.all.integrated, file = paste("output/",sfname,".rds",sep = ""))
+}
 
-data.all.integrated <- FindNeighbors(data.all.integrated, dims = 1:10)
+data.all.integrated <- FindNeighbors(data.all.integrated, dims = 1:15)
 data.all.integrated <- FindClusters(data.all.integrated, resolution = 0.5)
 data.all.integrated <- RunPCA(data.all.integrated, npcs = 30, verbose = FALSE)
-data.all.integrated <- RunUMAP(data.all.integrated, reduction = "pca", dims = 1:30)
+data.all.integrated <- RunUMAP(data.all.integrated, reduction = "pca", dims = 1:30, verbose= FALSE)
 data.all.integrated <- RunTSNE(data.all.integrated, dims = 1:30, perplexity=10,dim.embed = 2)
 saveRDS(data.all.integrated, file = paste("output/",sfname,"reduced.rds",sep = ""))
 
-pdf(file=paste("figs/",sfname,"_dimplots.pdf",sep = ""))
+pdf(file=paste("figs/",sfname,format(Sys.Date(),format = "%s"),"_dimplots.pdf",sep = ""))
 p1 <- DimPlot(data.all.integrated, reduction = "pca", group.by = "labels.files.num")
 p2 <- DimPlot(data.all.integrated, reduction = "umap", group.by = "labels.files.num")
 p3 <- DimPlot(data.all.integrated, reduction = "tsne", group.by = "labels.files.num")
 plot_grid(p1, p2,p3)
 dev.off()
 
-}# End else
+
+
+# End else
 
 #pdf(file=paste("figs/",sfname,"_markers_vlnplot.pdf",sep = ""))
 #VlnPlot(my.object, features = find_topk_marker(my.object.markers.all,k=9), slot = "counts", log = TRUE,pt.size=0)
