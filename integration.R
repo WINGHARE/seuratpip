eaf62414a9b28d110b8a7a4bbcba2ede2205bbdb
junyichen8 <junyichen8-c@my.cityu.edu.hk>
2019-07-11 13:40:29 -0400

    Optin

--- integration.R
+++ integration.R
@@ -14,9 +14,9 @@ option_list = list(
               help="dataset file names, seperated by commas", metavar="character"),
   make_option(c("-o", "--out"), type="character", default="out.txt", 
               help="output file name [default= %default]", metavar="character"),
-  make_option(c("-fl", "--filterl"), type ="integer", default=as.integer(500),
+  make_option("--filterl", type ="integer", default=as.integer(500),
               help="The lower bound of the filter nFerature RNA [default= %default]", metavar="number"),
-  make_option(c("-fr", "--filterr"), type ="integer", default=as.integer(5000), 
+  make_option("--filterr", type ="integer", default=as.integer(5000), 
               help="The upper bound of the filter nFerature RNA", metavar="number")
 )
 
