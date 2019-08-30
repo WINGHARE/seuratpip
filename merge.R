d1fbf30e9505dcfc7239b230ac76ea173722f014
Chen <che275@EEIAdmins-MBP.osumc.edu>
2019-08-30 09:03:21 -0400

    Edit seurat merge

--- merge.R
+++ merge.R
@@ -52,7 +52,6 @@ sfname <- paste(gsub("[.h5]|[.csv]|[data/]|[.txt]","",filenames[[1]]),"integrate
 if (file.exists(paste("output/",sfname,".rds", sep = ""))){
   data.all.integrated<-readRDS(file = paste("output/",sfname,".rds", sep = ""))
 }else{ # Start else
-
 # Load data files base on different type of files
 if(length(grep(".h5",filename)>0)){
     
@@ -66,60 +65,25 @@ if(length(grep(".h5",filename)>0)){
   my.raw.data <- Read10X(data.dir = filename)
   
 }
+}
 my.object <- list()
 
 # Preprocess the raw files
 for(i in 1:num.files){
   my.object[[i]]<- CreateSeuratObject(my.raw.data[[i]])
   my.object[[i]] <- RenameCells(my.object[[i]], add.cell.id = i)
-  my.object[[i]] <- NormalizeData(my.object[[i]],verbose = FALSE)
-  my.object[[i]]<-NormalizeData(my.object[[i]],normalization.method = "LogNormalize",
-                                scale.factor = 10000)
+  #my.object[[i]] <- NormalizeData(my.object[[i]],verbose = FALSE)
+  #my.object[[i]]<-NormalizeData(my.object[[i]],normalization.method = "LogNormalize",
+                                #scale.factor = 10000)
   
-  my.object[[i]] <- subset(my.object[[i]], subset = nFeature_RNA > opt$filterl & nFeature_RNA < opt$filterr)
-  my.object[[i]] <- FindVariableFeatures(my.object[[i]],selection.method = "vst"
-                                         ,nfeatures = 500, verbose =  FALSE)
+  #my.object[[i]] <- subset(my.object[[i]], subset = nFeature_RNA > opt$filterl & nFeature_RNA < opt$filterr)
+  #my.object[[i]] <- FindVariableFeatures(my.object[[i]],selection.method = "vst"
+                                         #,nfeatures = 500, verbose =  FALSE)
   
 }
-names(my.object) <- sfnames
-# Find anchors using of different files
-data.all.anchors <- FindIntegrationAnchors(my.object,dims=1:30)
-data.all.integrated <- IntegrateData(anchorset = data.all.anchors, dims = 1:30)
-DefaultAssay(data.all.integrated) <- "integrated"
 
-# Custering analysis for the integrated data
-data.all.integrated <- ScaleData(data.all.integrated, verbose = FALSE)
+data.all.integrated<- merge(my.object[[1]], y = c(pbmc4k, pbmc8k)
+                            , add.cell.ids = c("3K", "4K", "8K")
+                            , project = "Tregko")
 
-labels.files.num <- unlist(lapply(rownames(data.all.integrated@meta.data),
-                              FUN=fun<-function(x){return(substr(x,1,1))}))
-
-data.all.integrated@meta.data<-cbind(data.all.integrated@meta.data,labels.files.num)
-
-saveRDS(data.all.integrated, file = paste("output/",sfname,".rds",sep = ""))
-}
-
-data.all.integrated <- FindNeighbors(data.all.integrated, dims = 1:15)
-data.all.integrated <- FindClusters(data.all.integrated, resolution = 0.5)
-data.all.integrated <- RunPCA(data.all.integrated, npcs = 30, verbose = FALSE)
-data.all.integrated <- RunUMAP(data.all.integrated, reduction = "pca", dims = 1:30, verbose= FALSE)
-data.all.integrated <- RunTSNE(data.all.integrated, dims = 1:30, perplexity=10,dim.embed = 2)
-saveRDS(data.all.integrated, file = paste("output/",sfname,"reduced.rds",sep = ""))
-
-pdf(file=paste("figs/",sfname,format(Sys.Date(),format = "%s"),"_dimplots.pdf",sep = ""))
-p1 <- DimPlot(data.all.integrated, reduction = "pca", group.by = "labels.files.num")
-p2 <- DimPlot(data.all.integrated, reduction = "umap", group.by = "labels.files.num")
-p3 <- DimPlot(data.all.integrated, reduction = "tsne", group.by = "labels.files.num")
-plot_grid(p1, p2,p3)
-dev.off()
-
-
-
-# End else
-
-#pdf(file=paste("figs/",sfname,"_markers_vlnplot.pdf",sep = ""))
-#VlnPlot(my.object, features = find_topk_marker(my.object.markers.all,k=9), slot = "counts", log = TRUE,pt.size=0)
-#dev.off()
-
-#pdf(file=paste("figs/",sfname,"_markers_vlnplotvs.pdf",sep = ""))
-#VlnPlot(my.object, features = find_topk_marker_each(my.object.markers.each,1), slot = "counts", log = TRUE,pt.size=0)
-#dev.off()
+saveRDS(data.all.integrated, file = paste("output/",sfname,"combined.rds",sep = ""))
